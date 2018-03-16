a239319da4fee5655f6a12fa35ca3eb3e01b689f
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-03-16 09:20:29 -0500

    Matrix fix

---
+++ distance.m
@@ -0,0 +1,8 @@
+function [ distances ] = distance( rssi )
+%DISTANCE Summary of this function goes here
+%   Detailed explanation goes here
+distances = [];
+for i = 1:length(rssi)
+    distances(i) = 10.^((-rssi(i)+36)/20);
+end
+
--- rssi_simulation.m
+++ rssi_simulation.m
@@ -1,25 +1,28 @@
 %Anchor 1 coordinates 
 anchor1 = [0,0];
 %Anchor 2 coordinates 
-anchor2 = [0,10];
+anchor2 = [0,12];
 %Anchor 3 coordinates 
-anchor3 = [10,0];
+anchor3 = [12,0];
 %Anchor 4 coordinates 
-anchor4 = [10,10];
+anchor4 = [12,12];
 
-%Mobile coordinates
-mobile1 = [3,6];
+%Mobile coordinate
+mobile1 = [7,5];
 
-anchors = [anchor1;anchor2;anchor3;anchor4;mobile1];
-plot(anchors(:,1),anchors(:,2),'*')
+anchors = [anchor1;anchor2;anchor3;anchor4];
+nodes = [anchors;mobile1];
+clf('reset')
+plot(nodes(:,1),nodes(:,2),'*')
 xlabel('m')
 ylabel('m')
-xlim([-1 11])
-ylim([-1 11])
-dog = '  Dog';
+xlim([-1 13])
+ylim([-1 13])
+dog = strcat('  Dog: ','[',num2str(mobile1(1)),',',num2str(mobile1(2)),']');
 text(mobile1(1),mobile1(2),dog)
 grid on
 hold on
+rssi = [];
 
 for i = 1:4
     radius = norm(anchors(i,:)-mobile1);
@@ -27,10 +30,9 @@ for i = 1:4
     xval = xval+anchors(i,1);
     yval = yval+anchors(i,2);
     plot(xval,yval,'--')
-    txt = strcat('  Anchor',num2str(i));
+    txt = strcat('  Anchor',num2str(i),': ');
     text(anchors(i,1),anchors(i,2),txt)
+    rssi(i) = rssi_value(radius);
 end
 
-
-
-
+distances = distance(rssi);
